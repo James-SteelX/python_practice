a8b4a6675bc576b1e9a9a8bdd1e064b0f8088227
James <James.Steel@me.com>
2017-01-11 14:51:57 +0000

    guard clauses stopping plane in bad weather

--- src/airport.py
+++ src/airport.py
@@ -10,7 +10,11 @@ class Airport(object):
         return randint(1, 5)
 
     def clear_landing(self, plane):
+        if self.current_weather == 5:
+            return 'Stormy weather, it is unsafe to land'
         self.apron.append(plane)
 
     def allow_takeoff(self, plane):
+        if self.current_weather == 5:
+            return 'Stormy weather, it is unsafe to fly'
         self.apron.pop()
--- tests/airport_test.py
+++ tests/airport_test.py
@@ -15,14 +15,35 @@ def test_for_random_weather_number():
         assert airport.current_weather == 3
 
 def test_for_landing_plane():
-    airport = Airport()
-    plane = Plane()
-    airport.clear_landing(plane)
-    assert len(airport.apron) == 1
+    with mock.patch('src.airport.Airport.weather') as mock_weather:
+        mock_weather.return_value = (3)
+        airport = Airport()
+        plane = Plane()
+        airport.clear_landing(plane)
+        assert len(airport.apron) == 1
+
+def test_for_unsafe_landing():
+    with mock.patch('src.airport.Airport.weather') as mock_weather:
+        mock_weather.return_value = (5)
+        airport = Airport()
+        plane = Plane()
+        assert airport.clear_landing(plane) == 'Stormy weather, it is unsafe to land'
+        assert len(airport.apron) == 0
 
 def test_for_plane_take_off():
-    airport = Airport()
-    plane = Plane()
-    airport.clear_landing(plane)
-    airport.allow_takeoff(plane)
-    assert len(airport.apron) == 0
+    with mock.patch('src.airport.Airport.weather') as mock_weather:
+        mock_weather.return_value = (3)
+        airport = Airport()
+        plane = Plane()
+        airport.clear_landing(plane)
+        airport.allow_takeoff(plane)
+        assert len(airport.apron) == 0
+
+def test_for_unsafe_take_off():
+    with mock.patch('src.airport.Airport.weather') as mock_weather:
+        mock_weather.return_value = (5)
+        plane = Plane()
+        airport = Airport()
+        airport.apron.append(plane)
+        assert airport.allow_takeoff(plane) == 'Stormy weather, it is unsafe to fly'
+        assert len(airport.apron) == 1
