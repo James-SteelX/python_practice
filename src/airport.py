368b80fdaefca073feb4d0e1090aec110b30d8f5
James <James.Steel@me.com>
2017-01-11 14:33:06 +0000

    stubbed out random number generator

--- src/airport.py
+++ src/airport.py
@@ -1,7 +1,13 @@
+from random import randint
+
 class Airport(object):
 
     def __init__(self):
         self.apron = []
+        self.current_weather = self.weather()
+
+    def weather(self):
+        return randint(1, 5)
 
     def clear_landing(self, plane):
         self.apron.append(plane)
--- tests/airport_test.py
+++ tests/airport_test.py
@@ -1,9 +1,18 @@
 from src.airport import *
 from src.plane import *
+import mock
+from mock import Mock
+
 
 def test_for_empty_apron():
     airport = Airport()
     assert len(airport.apron) == 0
+
+def test_for_random_weather_number():
+    with mock.patch('src.airport.Airport.weather') as mock_weather:
+        mock_weather.return_value = (3)
+        airport = Airport()
+        assert airport.current_weather == 3
 
 def test_for_landing_plane():
     airport = Airport()
